3236b74eb3f4b235f7794a8a04d0640bd50563fb
subinio <subinjin22@gmail.com>
2021-02-06 09:23:14 +0900

    Add templates and static files
    1. Modified django app setting
    2. Add Templates filse and Static files
    3. Modified banner size
    4. Using bootstrap for frontend
    5. And we will add LICENSE of this bootstrap templates.

--- project/project/urls.py
+++ project/project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import tempApp.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', tempApp.views.index, name='index'),
 ]
